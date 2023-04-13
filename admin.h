b5bbd409a348dc38ff16d31ab06d5c4d7d88ff88
Erfan289 <121449303+Erfan289@users.noreply.github.com>
2023-04-12 22:49:38 -0700

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,12 @@
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+
+
+#endif // USER_H_INCLUDED
+static void addNewStudent();
+static void deleteStudent();
+static void showStudentRecords();
+static void editAdminPassword();
+static void editStudentGrade();
+void admin_mode();
